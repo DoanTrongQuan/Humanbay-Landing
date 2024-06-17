282c2387762623f4ce9a8d2b8347d9d78c3d5f98
TrongQuan <142089306+DoanTrongQuan@users.noreply.github.com>
2024-06-17 17:45:21 +0700

    init project

---
+++ components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>
+      <img src = "/img/logo-human.svg">
+    </div>
+    
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class = "container">
+    <Header/>
+    <div>
+      <div>
+        <img src = "/img/background-intro.png">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
